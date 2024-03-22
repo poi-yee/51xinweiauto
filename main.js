364a5b96382ab2cf1510e53c56e457fedb0e7ce7
PoiYee <70061715+poi-yee@users.noreply.github.com>
2024-03-22 18:31:49 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         芯位教育新界面自动下一集
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2.1
 // @description  新版界面的芯位自动下一集脚本（其他功能还没加（菜
-// @author       PoiYee
+// @author       PoiYee,Code-dogcreatior
 // @match        *://*.51xinwei.com/*
 // @icon         *://*.51xinwei.com/*
 // @grant        none
@@ -13,17 +13,39 @@
 (function() {
     'use strict';
 
+    // 点击下一章节按钮并模拟鼠标移动
+    function autoClickAndMove() {
+        var nextChapterButton = document.querySelector('.button-box > .left');
+        if (nextChapterButton) {
+            nextChapterButton.click();
+            console.log('Next chapter button clicked.');
+        }
+       // 模拟鼠标移动
+        var x = Math.random() * window.innerWidth;
+        var y = Math.random() * window.innerHeight;
+        window.scrollTo(x, y);
+        console.log('Mouse simulated at position (' + x + ', ' + y + ').');
+    }
+
+    // 每分钟执行一次点击和模拟鼠标移动
+   // setInterval(autoClickAndMove, 60000); // 60秒 * 1000 毫秒
+
+    // 使用 MutationObserver 监听 DOM 变化，以备不时之需
     var observer = new MutationObserver(function(mutations) {
         mutations.forEach(function(mutation) {
             if (mutation.addedNodes && mutation.addedNodes.length > 0) {
                 // element added to DOM
-                var hasNewNodes = Array.from(mutation.addedNodes).some(node => node.nodeType === 1);
-                if (hasNewNodes) {
-                    // check if a new node has the specific class and data attribute
-                    var element = document.querySelector('.left');
-                    if (element) {
-                        element.click();
-                        console.log('Element clicked.');
+                autoClickAndMove();
+
+                // 检查是否有视频元素
+                var videoElement = document.querySelector('video');
+                if (videoElement) {
+                    // 检查静音按钮的标题是否为 "静音"
+                    var muteButton = document.querySelector('.vjs-mute-control');
+                    if (muteButton && muteButton.getAttribute('title') === '静音') {
+                        // 如果静音按钮的标题为 "静音"，则点击它
+                        muteButton.click();
+                        console.log('Video muted.');
                     }
                 }
             }
@@ -36,4 +58,5 @@
     };
 
     observer.observe(document.body, config);
+
 })();
